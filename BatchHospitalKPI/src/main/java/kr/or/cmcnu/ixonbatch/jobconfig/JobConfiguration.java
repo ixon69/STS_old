ca2760750731d47e2c3b1c7b66e0fed931a241a6
ixon69 <ixon69@gmail.com>
2020-09-01 14:38:46 +0900

    첫번째 완성 샘플

--- BatchHospitalKPI/src/main/java/kr/or/cmcnu/ixonbatch/jobconfig/JobConfiguration.java
+++ BatchHospitalKPI/src/main/java/kr/or/cmcnu/ixonbatch/jobconfig/JobConfiguration.java
@@ -5,14 +5,18 @@ import java.util.Map;
 
 import javax.sql.DataSource;
 
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.batch.MyBatisBatchItemWriter;
 import org.mybatis.spring.batch.MyBatisCursorItemReader;
 import org.mybatis.spring.batch.MyBatisPagingItemReader;
+import org.mybatis.spring.batch.builder.MyBatisBatchItemWriterBuilder;
 import org.mybatis.spring.batch.builder.MyBatisCursorItemReaderBuilder;
 import org.mybatis.spring.batch.builder.MyBatisPagingItemReaderBuilder;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.item.ItemProcessor;
@@ -20,12 +24,14 @@ import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Configuration
 @EnableBatchProcessing
 public class JobConfiguration {
@@ -42,36 +48,36 @@ public class JobConfiguration {
     }
 
     @Bean
-    protected Step step1(ItemReader<String> reader,
+    protected Step step1(ItemReader<HashMap> reader,
 //    						ItemProcessor<HashMap, HashMap> processor,
     						ItemWriter<HashMap> writer) {
         return steps.get("step1")
-            .<String, HashMap> chunk(10)
+            .<HashMap, HashMap> chunk(10)
             .reader(reader)
 //          .processor(itemProcessor)
             .writer(writer)
             .build();
-    }    
+    }
     
     @Bean
-    public MyBatisCursorItemReader<String> reader() {
-        return new MyBatisCursorItemReaderBuilder<String>()
-        			.sqlSessionFactory(sqlSessionFactory())
-        			.queryId("com.my.name.space.batch.EmployeeMapper.getEmployee")
+    @JobScope
+    public MyBatisCursorItemReader<HashMap> reader(@Value("#{jobParameters[department]}") String department, @Qualifier("sSqlSessionFactory") SqlSessionFactory sqlSessionFactory) {
+    	Map<String, Object> parameterValues = new HashMap<>();
+    	parameterValues.put("department", department);
+        return new MyBatisCursorItemReaderBuilder<HashMap>()
+        			.sqlSessionFactory(sqlSessionFactory)
+        			.queryId("SourceMapper.selectPhoneInside")
+        			.parameterValues(parameterValues)
         			.build();
     }
 
     @Bean
-    public ItemWriter<Bill> jdbcBillWriter(DataSource dataSource) {
-        JdbcBatchItemWriter<Bill> writer = new JdbcBatchItemWriterBuilder<Bill>()
-                        .beanMapped()
-                .dataSource(dataSource)
-                .sql("INSERT INTO BILL_STATEMENTS (id, first_name, " +
-                   "last_name, minutes, data_usage,bill_amount) VALUES " +
-                   "(:id, :firstName, :lastName, :minutes, :dataUsage, " +
-                   ":billAmount)")
-                .build();
-        return writer;
+    @StepScope
+    public MyBatisBatchItemWriter<HashMap> writer(@Qualifier("tSqlSessionFactory") SqlSessionFactory sqlSessionFactory) {
+    	return new MyBatisBatchItemWriterBuilder<HashMap>()
+    				.sqlSessionFactory(sqlSessionFactory)
+    				.statementId("TargetMapper.insertPhoneInside")
+    				.build();
     }
     
 }
