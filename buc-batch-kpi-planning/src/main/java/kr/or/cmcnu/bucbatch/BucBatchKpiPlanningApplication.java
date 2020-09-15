07516a6a32d185556be08cb5f101e5bd8415d550
ixon69 <ixon69@gmail.com>
2020-09-16 08:07:13 +0900

    initial commit & push

---
+++ buc-batch-kpi-planning/src/main/java/kr/or/cmcnu/bucbatch/BucBatchKpiPlanningApplication.java
@@ -0,0 +1,13 @@
+package kr.or.cmcnu.bucbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BucBatchKpiPlanningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BucBatchKpiPlanningApplication.class, args);
+	}
+
+}
